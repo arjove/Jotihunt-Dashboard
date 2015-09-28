8222653d0107f5d9522c8ecc18118b57a00ae1b2
Arjo <anyone0481@gmail.com>
2015-09-28 12:36:42 +0200

    adding new things.

---
+++ application/core/Log.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Log
+ * This class handles everything that is done for the logging of a users actions.
+ */
+class Log {
+    /**
+     * Get all the log enties that are in the database.
+     * @param int $limit
+     * @param bool $lastlogin
+     * @return array
+     */
+    public static function getLog($limit = 200, $lastlogin = false) {
+        $db = DatabaseFactory::getFactory()->fluent();
+
+        if($lastlogin) {
+            $user_last_login = UserModel::getUserLastLoginTimestamp();
+            $query = $db->from('log')->limit($limit)->where("ENTRY_ID > :timestamp", array(':timestamp' => $user_last_login))->orderBy('ENTRY_ID DESC'); ;
+        } else {
+            $query = $db->from('log')->limit($limit);
+        }
+        $query->execute();
+        $data = $query->fetchall();
+
+        return json_decode(json_encode($data), true);
+    }
+
+    /**
+     * @param int $limit
+     * @return mixed
+     */
+    public static function createTimeLine($limit = 100) {
+        $db = DatabaseFactory::getFactory()->fluent();
+        $query = $db->from('log')->limit($limit)->orderBy('ENTRY_ID DESC');
+        $query->execute();
+        $result = $query->fetchAll();
+
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Get Activity of a single user.
+     * @param int $user_id id of the specific user
+     * @return object a single object (the result)
+     */
+    public static function getActivity($user_id) {
+       //
+        return false;
+    }
+
+    /**
+     * @param $type
+     * @param $title
+     * @param $text
+     * @param null $user_id
+     * @param null $param
+     * @return bool
+     */
+    public static function log($type, $title, $text, $user_id = null, $param = null) {
+        $db = DatabaseFactory::getFactory()->fluent();
+
+        $values = array(
+            'ENTRY_ID' => time(),
+            'type' => $type,
+            'title' => $title,
+            'text' => $text,
+            'user_id' => $user_id,
+            'param' => json_encode($param)
+        );
+
+        $query = $db->insertInto('log')->values($values);
+        if($query->execute() === true) {
+            return true;
+        } else {
+            Session::error('The Log system failed to log a log message.'.time().' | '.$title);
+        }
+    }
+}
---
+++ application/model/DataModel.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Handles all data manipulation of the admin part
+ */
+class DataModel
+{
+
+    public static function updateLastUpdate($type, $value){
+
+
+        $db = DatabaseFactory::getFactory()->fluentPDO();
+        $query = $db->insertInto('item', $values);
+        if($query->execute()) {
+            LogModel::log('success', 'A item was added', $name, Session::get('user_id'));
+            return true;
+        } else {
+            LogModel::log('error', 'Failed to add a item', $name, Session::get('user_id'));
+            return false;
+        }
+
+        // just do nothing for now.
+        return false;
+    }
+
+}
