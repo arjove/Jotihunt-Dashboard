497b7656705f782a69b1fbcbe3c649acd4b70b8d
Arjo <anyone0481@gmail.com>
2015-10-06 15:17:56 +0200

    big update to update database. probally does fix things.

--- application/model/GameModel.php
+++ application/model/GameModel.php
@@ -38,20 +38,160 @@ class GameModel
         $query = $db->update('settings')->set($set)->where('KEY', 'updateInterval');
         if ($query->execute()){
             Log::log('success', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $set);
+            return true;
         } else {
             Log::log('error', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $set);
+            return false;
         }
-        return false;
     }
 
+    private static function setLastUpdated($type, $updatevalue) {
+        $db = DatabaseFactory::getFactory()->fluent();
+        $query = $db->deleteFrom('update_data', array('id' , $type));
+        if ($query->execute()){
+            Log::log('success', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), null);
+            $values = array(
+                'UUID' => null,
+                'id' => $type,
+                'time' => $updatevalue
+            );
+            $query = $db->insertInto('update_data', $values);
+            if ($query->execute()){
+                Log::log('success', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                return true;
+            } else {
+                Log::log('error', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                return false;
+            }
+        } else {
+            Log::log('error', 'UpdateSetting', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), null);
+            return false;
+        }
+    }
     /**
      * @param $type
      * @return bool
      */
-    public static function updateDatabase($type){
-        // pulls the type
+    public static function updateDatabase($type, $id){
+        $db = DatabaseFactory::getFactory()->fluent();
+        if (!$type) {
+            if ($type = 'opdracht') {
+                $data = self::buildLink($type);
+                foreach ($data['data'] as $key => $value) {
+                    $values = array(
+                        'UUID' => $value['ID'],
+                        'title' => $value['titel'],
+                        'start' => $value['datum'],
+                        'end' => $value['eindtijd'],
+                        'max_points' => $value['maxpunten'],
+                        'finished' => false,
+                    );
 
-        // just do nothing for now.
+                    $database = DatabaseFactory::getFactory()->getConnection();
+
+                    $sql = "SELECT UUID FROM opdrachten WHERE UUID = :uuid ";
+                    $query = $database->prepare($sql);
+                    $query->execute(array(':uuid' => $values['id']));
+
+
+                    if ($query->rowCount() > 0) {
+                        $query = $db->update('opdrachten')->set($values)->where('UUID', $values['ID']);
+                        if ($query->execute()) {
+                            Log::log('success', 'updateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'UpdateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    } elseif ($query->rowCount() == 0) {
+                        $query = $db->insertInto('opdrachten', $values);
+                        if ($query->execute()) {
+                            Log::log('success', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    }
+                    unset($query);
+                    unset($sql);
+
+                }
+
+            } elseif ($type = 'hint') {
+                $data = self::buildLink($type);
+                foreach ($data['data'] as $key => $value) {
+                    $values = array(
+                        'UUID' => $value['ID'],
+                        'title' => $value['titel'],
+                        'start' => $value['datum'],
+                        'end' => $value['eindtijd'],
+                        'max_points' => $value['maxpunten'],
+                        'finished' => false,
+                    );
+
+                    $database = DatabaseFactory::getFactory()->getConnection();
+
+                    $sql = "SELECT UUID FROM opdrachten WHERE UUID = :uuid ";
+                    $query = $database->prepare($sql);
+                    $query->execute(array(':uuid' => $values['id']));
+
+
+                    if ($query->rowCount() > 0) {
+                        $query = $db->update('opdrachten')->set($values)->where('UUID', $values['ID']);
+                        if ($query->execute()) {
+                            Log::log('success', 'updateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'UpdateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    } elseif ($query->rowCount() == 0) {
+                        $query = $db->insertInto('opdrachten', $values);
+                        if ($query->execute()) {
+                            Log::log('success', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    }
+                    unset($query);
+                    unset($sql);
+
+                }
+
+            } elseif ($type = 'nieuws') {
+                $data = self::buildLink($type);
+                foreach ($data['data'] as $key => $value) {
+                    $values = array(
+                        'UUID' => $value['ID'],
+                        'title' => $value['titel'],
+                        'date' => $value['datum']
+                    );
+
+                    $database = DatabaseFactory::getFactory()->getConnection();
+
+                    $sql = "SELECT UUID FROM opdrachten WHERE UUID = :uuid ";
+                    $query = $database->prepare($sql);
+                    $query->execute(array(':uuid' => $values['id']));
+
+
+                    if ($query->rowCount() > 0) {
+                        $query = $db->update('opdrachten')->set($values)->where('UUID', $values['ID']);
+                        if ($query->execute()) {
+                            Log::log('success', 'updateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'UpdateRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    } elseif ($query->rowCount() == 0) {
+                        $query = $db->insertInto('opdrachten', $values);
+                        if ($query->execute()) {
+                            Log::log('success', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        } else {
+                            Log::log('error', 'insertRow', 'Setting UpdateInterval was updated', 'Update interval was updated', Session::get('user_id'), $values);
+                        }
+                    }
+                    unset($query);
+                    unset($sql);
+
+                }
+
+            }
+            self::setLastUpdated($type, $data['last_update']);
+        }
         return false;
     }
 
